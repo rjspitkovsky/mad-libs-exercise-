aac3de3b44b22f225baa5e52652a508fa754383e
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-07-27 15:56:47 -0400

    first mad libs requests to command line set

---
+++ mad_libs.py
@@ -0,0 +1,9 @@
+def collect_words():
+    adjective = raw_input("Name an adjective ")
+    pet_name = raw_input("Name a tiny pet's name ")
+    verb = raw_input("Name a verb ")
+    president = raw_input("Name a former President ")
+    lunch = raw_input("Name a meal for lunch ")
+    dollars = raw_input("Name a dollar amount (no need to use $ sign) ")
+
+collect_words()
